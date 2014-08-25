c5ca2d94e0a62c6480269ce8ccd208d8faa6fa4c
Scott Baldwin <scott@wellmatchhealth.com>
2014-08-25 17:13:49 -0500

    Adding calculator functions

--- app/models/metric_calculator.rb
+++ app/models/metric_calculator.rb
@@ -1,11 +1,24 @@
 class MetricCalculator
-  def self.long_term_average(data, key)
+  def self.average_of_values(values)
+    total = values.inject(0.0) { |sum, el| sum + el }
     average = 0
-    total = data.inject(0.0) do |sum, el|
-      el.symbolize_keys!
-      sum + el[key].to_f
-    end
-    average = total / data.count if data.count > 0
+    average = total / values.count if values.count > 0
     average
+  end
+
+  def self.average_arrival_rate_for_list(list_id = 'list_not_set')
+    values = CardActivity.select("count(id) as time_in_list").group("grouping_year, grouping_week").where(list_id: list_id).where.not(exit_date: nil).map(&:time_in_list)
+    average = average_of_values values
+    average
+  end
+
+  def self.average_cycle_time_for_list(list_id = 'list_not_set')
+    values = CardActivity.select("sum(time_in_list) as time_in_list").group("grouping_year, grouping_week").where(list_id: list_id).map(&:time_in_list)
+    average = average_of_values values
+    average
+  end
+
+  def self.average_wip_for_list(list_id = 'list_not_set')
+    average_cycle_time_for_list(list_id) * average_arrival_rate_for_list(list_id)
   end
 end
--- spec/models/metric_calculator_spec.rb
+++ spec/models/metric_calculator_spec.rb
@@ -2,14 +2,9 @@ require 'spec_helper'
 
 describe MetricCalculator do
   it "should calculate the long term average" do
-    data = [
-      {"grouping_year"=>"2014", "grouping_week"=>"32", "total"=>"30"},
-      {"grouping_year"=>"2014", "grouping_week"=>"33", "total"=>"35"},
-      {"grouping_year"=>"2014", "grouping_week"=>"34", "total"=>"40"},
-      {"grouping_year"=>"2014", "grouping_week"=>"35", "total"=>"45"}
-    ]
+    data = [10, 2, 2]
 
-    long_term_average = MetricCalculator.long_term_average(data, :total)
-    expect(long_term_average).to eq(37.5)
+    long_term_average = MetricCalculator.average_of_values(data)
+    expect(long_term_average.round(1)).to eq(4.7)
   end
 end
